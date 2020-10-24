568c154f5c8b9c374ec601a604e42ae60c9c4575
Gui Bruni <gdgcbruni@gmail.com>
2020-10-24 08:56:41 -0300

    Renamede some files

--- FractalTree.v2/sketch.js
+++ FractalTree.v2/sketch.js
@@ -1,42 +1,40 @@
 var angle = 0
-var rnd1;
-var rnd2;
 
 var slider;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  rnd1 = random(0.5, 0.75);
-  rnd2 = random(0.5, 0.75);
-  //slider = createSlider(0, TWO_PI, PI/4, 0.01);
-}
-function mouseClicked(){
-  setup();
+  slider = createSlider(0, TWO_PI, PI/4, 0.01);
 }
 
 function draw() {
   background(0);
-  angle = sin(millis()/3000);
+  angle = slider.value()
   stroke(255);
-  translate(windowWidth/2, windowHeight);
-  branch(windowHeight/6);
+  translate(windowWidth/2, height);
+  branch(300);
   
 }
 
 function branch(len) {
   line(0, 0, 0, -len);
   translate(0, -len)
-  if ( len > 4) {
+  if ( len > 16) {
     push();
   rotate(angle)
-    branch(len * rnd1)
+    branch(len * 0.65)
     pop();
     push();
   rotate(-angle);
-    branch(len * rnd2)
+    branch(len * 0.65)
+    pop();
+    push();
+  rotate(2*angle);
+    branch(len * 0.65)
     pop();
   } 
   
   
   
   }
+
